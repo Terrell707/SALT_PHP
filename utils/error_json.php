f1d7d22034df416e3a3fc701bf9d4d869ffdf539
Terrell707 <adr510909@gmail.com>
2015-02-07 19:42:38 -0800

    Return error status as json array

--- utils/error_json.php
+++ utils/error_json.php
@@ -1,9 +1,16 @@
 <? // Handles error messages
 
 function error($error_code, $error_message) {
+  // Creates an empty array so that json will be returned as an array.
+  $json = array();
+
+  // Creates key-value pairs for the error_code.
   $error['error_code'] = $error_code;
   $error['error_message'] = $error_message;
-  $json = json_encode($error);
+
+  // Push the key-value pairs and creates a json object out of it.
+  array_push($json, $error);
+  $json = json_encode($json);
   return $json;
 }
 
