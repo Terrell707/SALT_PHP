e467766673d346e960d93fda62fb7e6659b1eec1
Terrell707 <adr510909@gmail.com>
2015-03-26 18:27:47 -0700

    Add function to prepend error code/message to the data

--- utils/error_json.php
+++ utils/error_json.php
@@ -10,7 +10,23 @@ function error($error_code, $error_message) {
 
   // Push the key-value pairs and creates a json object out of it.
   array_push($json, $error);
+
+  // Turns the array into a json object.
   $json = json_encode($json);
+  return $json;
+}
+
+function success($data, $error_code, $error_message) {
+  // Creates an array out of the error code and message.
+  $error = array();
+
+  // Creates key-value pairs for the error_code.
+  $error['error_code'] = $error_code;
+  $error['error_message'] = $error_message;
+
+  // Places the error code at the front of the data and returns it.
+  array_unshift($data, $error);
+  $json = json_encode($data);
   return $json;
 }
 
